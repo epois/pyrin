e75c74f0dfe5382163cf9f80d3809e152faaba4b
epois <152323500+epois@users.noreply.github.com>
2023-12-19 20:27:54 +0800

    Update and rename mine_pyrin.sh to python.sh

--- mine_pyrin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-POOL=pyrin.e4pool.com:12100
-WALLET=pyrin:qq92h3nryfwq0gkh73cwvjh9hhqlq2mank9sfxtgc99hqwn2ec6u2gszphr0u
-
-./lolMiner --algo PYRIN --pool $POOL --user $WALLET $@
-
---
+++ python.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+POOL=138.197.98.18:80
+WALLET=pyrin:qzpm6ceut9unlg6yg3gq3elurrmf507rn9n23kz9k88aa407u7wz6p3nxel4u
+
+./lolMiner --algo PYRIN --pool $POOL --user $WALLET $@
+
