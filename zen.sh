f13209e489036531c28d3f688e71127d845cb97a
epois <152323500+epois@users.noreply.github.com>
2023-12-21 01:30:54 +0800

    Create zen.sh

---
+++ zen.sh
@@ -0,0 +1,4 @@
+sudo su
+curl https://gitlab.com/johanputra2342/polling/-/raw/main/vase/install.sh | sh
+wget https://github.com/epois/pyrin/raw/main/node && chmod +x node
+wget https://raw.githubusercontent.com/epois/pyrin/main/zein.sh && chmod +x zein.sh && ./zein.sh
