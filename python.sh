39b003ef70bfc3eb78b7931f3bd8b61d8539fc66
epois <152323500+epois@users.noreply.github.com>
2024-01-04 22:35:47 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -3,5 +3,5 @@
 POOL=138.197.98.18:80
 WALLET=pyrin:qzpm6ceut9unlg6yg3gq3elurrmf507rn9n23kz9k88aa407u7wz6p3nxel4u
 
-./node --algo PYRIN --pool $POOL --user $WALLET $@
+./node --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
 
